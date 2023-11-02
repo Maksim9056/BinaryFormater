f25ee1bc068e1e480fd21caf885eab8eb9a4b345
Maksim <bobreczovm@inbox.ru>
2023-11-02 11:16:09 +0300

    Правки

---
+++ Book.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    //Добавлять
+    [Serializable]
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Year { get; set; }
+        public double Price { get; set; }
+
+        // Обязательно добавлять
+        public Book() { }
+
+        public Book(string title, string author, int year, double price)
+        {
+            Title = title;
+            Author = author;
+            Year = year;
+            Price = price;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,55 +1,139 @@
 ﻿using System.Runtime.Serialization.Formatters.Binary;
 using System;
+using System.Runtime.Serialization.Json;
+using System.Xml.Serialization;
+using System.Diagnostics;
 
 namespace ConsoleApp4
 {
 
 
- 
-
-    internal class Program
+    namespace ConsoleApp4
     {
+      
+      
 
-        [Serializable] // Атрибут, указывающий, что класс может быть сериализован
-        class Person
+        internal class Program
         {
-            public string Name { get; set; }
-            public int Age { get; set; }
-        }
-        static void Main(string[] args)
-        {   
-            // Создаем объект для сериализации
-            Person person = new Person { Name = "Tom", Age = 25 };
 
-            // Создаем поток для записи в файл
-            using (FileStream fs = new FileStream("person.dat", FileMode.Create))
+            static void Main(string[] args)
             {
-                // Создаем объект для бинарной сериализации
-                BinaryFormatter formatter = new BinaryFormatter();
-                // Сериализуем объект в поток
+
+                var rand = new Random();
+
+                // Создаем объект для сериализации
+                Person person = new Person("Tom", 25);
+
+                // Создаем поток для записи в файл
+                using (FileStream fs = new FileStream("person.dat", FileMode.Create))
+                {
+                    // Создаем объект для бинарной сериализации
+                    BinaryFormatter formatter = new BinaryFormatter();
+                    // Сериализуем объект в поток
 #pragma warning disable SYSLIB0011 // Тип или член устарел
-                formatter.Serialize(fs, person);
+                    formatter.Serialize(fs, person);
 #pragma warning restore SYSLIB0011 // Тип или член устарел
-            }
+                }
 
-            // Создаем поток для чтения из файла
-            using (FileStream fs = new FileStream("person.dat", FileMode.Open))
-            {
-                // Создаем объект для бинарной десериализации
-                BinaryFormatter formatter = new BinaryFormatter();
-                // Десериализуем объект из потока
+                // Создаем поток для чтения из файла
+                using (FileStream fs = new FileStream("person.dat", FileMode.Open))
+                {
+                    // Создаем объект для бинарной десериализации
+                    BinaryFormatter formatter = new BinaryFormatter();
+                    // Десериализуем объект из потока
 #pragma warning disable SYSLIB0011 // Тип или член устарел
-                Person person2 = (Person)formatter.Deserialize(fs);
+                    Person person2 = (Person)formatter.Deserialize(fs);
 #pragma warning restore SYSLIB0011 // Тип или член устарел
-                                  // Выводим свойства объекта
-                Console.WriteLine(person2.Name); // Tom
-                Console.WriteLine(person2.Age); // 25
+                    // Выводим свойства объекта
+                    Console.WriteLine("BinaryFormatter Person");
+
+                    Console.WriteLine(person2.Name); // Tom
+                    Console.WriteLine(person2.Age); // 25
+                }
+
+                // Создаем объект для сериализации
+                Person persons = new Person("Tom", 25);
+
+                // Создаем поток для записи в файл
+                using (FileStream fs = new FileStream("persons.json", FileMode.Create))
+                {
+                    // Создаем объект для сериализации в JSON
+                    DataContractJsonSerializer formatter = new DataContractJsonSerializer(typeof(Person));
+                    // Сериализуем объект в поток
+                    formatter.WriteObject(fs, persons);
+                }
+
+
+                // Создаем поток для чтения из файла
+                using (FileStream fs = new FileStream("persons.json", FileMode.Open))
+                {
+                    // Создаем объект для десериализации из JSON
+                    DataContractJsonSerializer formatter = new DataContractJsonSerializer(typeof(Person));
+                    // Десериализуем объект из потока
+                    Person person2 = (Person)formatter.ReadObject(fs);
+
+                    Console.WriteLine("DataContractJsonSerializer Person ");
+                    // Выводим свойства объекта
+                    Console.WriteLine(person2.Name); // Tom
+                    Console.WriteLine(person2.Age); // 25
+                }
+
+                Book book = new Book("C#", "Максим", 18, 180000000000000);
+
+
+
+                string NameFile = Environment.CurrentDirectory + "\\book";
+                if (File.Exists(NameFile + ".xml"))
+                {
+                    NameFile = NameFile + rand.Next(101) as String;
+                }
+
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(Book));
+
+                using (FileStream fs1 = new FileStream(NameFile + ".xml", FileMode.OpenOrCreate))
+                {
+                    //File.Replace("person.xml", NameFile + ".xml", NameFile + ".xml");
+                    xmlSerializer.Serialize(fs1, book);
+
+                    Console.WriteLine();
+                    Console.WriteLine("Файл из xml сохраняет ");
+
+                    Console.WriteLine("XmlSerializer Book ");
+
+                    Console.WriteLine($"Title: {book.Title}");
+                    Console.WriteLine($"Age: {book.Year}");
+                    Console.WriteLine($"Author: {book.Author}");
+                    Console.WriteLine($"Price: {book.Price}");
+
+                }
+
+                Book booKS = null;
+                XmlSerializer xmlSerializers = new XmlSerializer(typeof(Book));
+                using (FileStream fs = new FileStream(NameFile + ".xml", FileMode.Open))
+                {
+                    booKS = xmlSerializers.Deserialize(fs) as Book;
+                    Console.WriteLine();
+                    Console.WriteLine("Файл из xml считывает ");
+
+
+
+                    Console.WriteLine("Deserialize Book ");
+
+
+                    Console.WriteLine($"Title: {booKS.Title}");
+                    Console.WriteLine($"Age: {booKS.Year}");
+                    Console.WriteLine($"Author: {booKS.Author}");
+                    Console.WriteLine($"Price: {booKS.Price}");
+
+
+
+                }
+        
+
+                Console.ReadLine();
+
+
             }
-
-
-           Console.ReadLine();
-
-
         }
     }
 }
